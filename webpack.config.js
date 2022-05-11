f31a698b35077376688db67a52c1aba5b314c720
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-05-11 12:40:29 +0100

    Add dev branch

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/Full-stack-developer/",
+  },
+  devServer: {
+    contentBase: [__dirname + "/src", __dirname + "/images"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+  },
+};
