73370e3ff185775ee65c7d8c81e6e8adc9557ecd
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-05-13 14:24:50 +0100

    Add data to api and retrieve it

--- src/render.js
+++ src/render.js
@@ -1,11 +1,11 @@
-import leaderBoard from './leaderBoard.js';
-import { renderSection } from './variables.js';
+import leaderBoard from "./leaderBoard.js";
+import { renderSection } from "./variables.js";
 
-async function render() {
+const render = async () => {
   const scoreOfPlayers = await leaderBoard.refreshScore();
-  renderSection.innerHTML = '';
+  renderSection.innerHTML = "";
   for (let i = 0; i < scoreOfPlayers?.length; i += 1) {
     renderSection.innerHTML += `<li> ${scoreOfPlayers[i].user} : ${scoreOfPlayers[i].score} </li>`;
   }
-}
-export default render;
+};
+export { render };
--- src/submit.js
+++ src/submit.js
@@ -1,14 +1,16 @@
-import leaderBoard from './leaderBoard.js';
-import render from './render.js';
+import leaderBoard from "./leaderBoard.js";
+import render from "./render.js";
 
-export default async function submit(userName, userScore) {
+const submit = async (userName, userScore) => {
   const scoreParseToInt = parseInt(userScore.value, 10);
-  if (userName.value !== '' && userScore.value !== '' && scoreParseToInt) {
+  if (userName.value !== "" && userScore.value !== "" && scoreParseToInt) {
     await leaderBoard.submitScore(userName.value, userScore.value);
-    userName.value = '';
-    userScore.value = '';
+    userName.value = "";
+    userScore.value = "";
   } else {
-    userName.value = '';
-    userScore.value = '';
+    userName.value = "";
+    userScore.value = "";
   }
-}
+};
+
+export { submit };
