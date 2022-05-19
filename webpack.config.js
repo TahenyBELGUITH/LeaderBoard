2414ae72d42eafc84d08d1db3fe3544546c47809
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-05-19 20:44:33 +0100

    Update deploying

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,46 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
-  },
-  devServer: {
-    static: './dist',
-  },
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
       },
     ],
   },
+  entry: "./src/index.js",
+  devServer: {
+    static: "./dist",
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "src/index.html",
+      title: "to-Do-List",
     }),
   ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+    publicPath: "/to-Do-list/",
+  },
 };
